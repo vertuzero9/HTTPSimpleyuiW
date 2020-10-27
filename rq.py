2e2c67d34607b2fb14598ec176189bf78d88af43
yuiW <38450714+vertuzero9@users.noreply.github.com>
2020-10-27 23:43:28 +0700

    Add files via upload

--- rq.py
+++ rq.py
@@ -1,25 +1,19 @@
-#!/bin/python2
+#!/bin/python3
 import sys
 import requests
 
-def beautiful(content):
-	return content.replace(',', '\n')
-
 def main(cookie, ts, token):
-	url = "http://www.xsslabelgg.com/action/friends/add?friend=44&__elgg_ts={0}&__elgg_token={1}&__elgg_ts={0}&__elgg_token={1}".format(ts, token)
-        print(url)
+	url = "http://www.xsslabelgg.com/action/friends/add?friend=44" + "&__elgg_ts={0}&__elgg_token={1}".format(ts, token)*2
 	headers = {
 		"Accept": "application/json, text/javascript, */*; q=0.01",
 	        "Cookie": "Elgg={0}".format(cookie),
 	        "Host": "www.xsslabelgg.com",
 	        "X-Requested-With": "XMLHttpRequest"
 	}
-
 	response = requests.get(url, headers=headers)
 	return response
 
 if __name__ == "__main__":
 	if len(sys.argv) == 4:
 		rs = main(sys.argv[1], sys.argv[2], sys.argv[3])
-		print(rs)
-		print(beautiful(rs.content))
+		print(rs, "\n\n", str(rs.content).replace(',', '\n'))
